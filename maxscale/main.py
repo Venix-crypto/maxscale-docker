7a16b6df1191dcc22adc0d2f4555cf87859d6b89
Venix-crypto <71543640+Venix-crypto@users.noreply.github.com>
2023-06-21 22:09:45 -0700

    Update main.py

--- maxscale/main.py
+++ maxscale/main.py
@@ -10,24 +10,24 @@ cnx = mysql.connector.connect(
 
 cursor = cnx.cursor()
 
-query1 = "SELECT MAX(zipcode) FROM zipcodes1"
+query1 = "SELECT MAX(zipcode) FROM zipcodes_one"
 cursor.execute(query1)
 largest_zipcode = cursor.fetchone()[0]
 print(largest_zipcode)
 
-query2 = "SELECT zipcode FROM zipcodes1 WHERE state='KY'"
+query2 = "SELECT zipcode FROM zipcodes_one WHERE state='KY'"
 cursor.execute(query2)
 ky_zipcodes = cursor.fetchall()
 for zipcode in ky_zipcodes:
     print(zipcode[0])
 
-query3 = "SELECT zipcode FROM zipcodes1 WHERE zipcode BETWEEN 40000 AND 41000"
+query3 = "SELECT zipcode FROM zipcodes_one WHERE zipcode BETWEEN 40000 AND 41000"
 cursor.execute(query3)
 zipcodes_range = cursor.fetchall()
 for zipcode in zipcodes_range:
     print(zipcode[0])
 
-query4 = "SELECT TotalWages FROM zipcodes1 WHERE state='PA'"
+query4 = "SELECT TotalWages FROM zipcodes_one WHERE state='PA'"
 cursor.execute(query4)
 pa_wages = cursor.fetchall()
 for wage in pa_wages:
